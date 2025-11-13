3e43e946bda2a6b9db61bd647e9a997aa1d5b0b9
rafael-iste <rafael.iste--gaie@edu.esiee.fr>
2025-11-13 13:38:39 +0000

    Solution exercice 05-palindromes

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-#### Fonction secondaire
+"""05-palindromes
+Permet de vérifier si une chaine est un palindrome ou pas.
+"""
 
+def ispalindrome(s):
+    """Vérifie si une chaîne est un palindrome ou pas.
 
-def ispalindrome(p):
+    @param s: Chaîne de caractères à tester.
+    @type s: str
+    @return: True si la chaîne est un palindrome, False sinon.
+    @rtype: bool
+    """
 
-    # votre code ici
-    
-    return False
+    s_clean = s.lower()
 
-#### Fonction principale
+    not_alpha = "àâäéèêëïîôöùûüçñ"
+    to_alpha = "aaaeeeeiioouuucn"
 
+    not_alpha_char = [char for char in s if not char.isalpha() and not char.isalnum()]
+    for char in not_alpha_char:
+        not_alpha+=char
+        to_alpha+=" "
+
+    clean_trans = str.maketrans(not_alpha, to_alpha)
+
+    s_clean = s_clean.translate(clean_trans).replace(" ", "")
+    return s_clean == s_clean[::-1]
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Fonction principale"""
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
-        print(s, ispalindrome(s))
-
+        print(f"{s}: {ispalindrome(s)}")
 
 if __name__ == "__main__":
     main()
